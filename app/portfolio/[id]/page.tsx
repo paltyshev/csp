8c450d89af9dc035372f8982e93e999b190136d1
paltyshev <aleksep@gmail.com>
2025-03-01 17:20:19 +0300

    Fix: удаление конфликтов слияния Git в динамическом маршруте

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -1,17 +1,7 @@
 import { Suspense } from 'react'
 import ProjectClientContainer from '@/components/ProjectClientContainer'
 
-<<<<<<< HEAD
 export default async function ProjectPage({ params }: { params: any }) {
-=======
-interface ProjectPageProps {
-  params: {
-    id: string
-  }
-}
-
-export default async function ProjectPage({ params }: ProjectPageProps) {
->>>>>>> 945d3fd0ea86772d6b7ffa40a01286e755180fda
   // Обработка параметров в серверном компоненте
   // В Next.js 15 нужно ждать params перед доступом к свойствам
   const resolvedParams = await params;
