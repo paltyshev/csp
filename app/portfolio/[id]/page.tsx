b6a4527062e1bdef0fc43d84664f05e825ae01e5
paltyshev <aleksep@gmail.com>
2025-03-01 17:22:22 +0300

    Fix: отключение правила ESLint о запрете any для динамических маршрутов

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from 'react'
 import ProjectClientContainer from '@/components/ProjectClientContainer'
 
+// Отключаем правило ESLint для этого файла
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export default async function ProjectPage({ params }: { params: any }) {
   // Обработка параметров в серверном компоненте
   // В Next.js 15 нужно ждать params перед доступом к свойствам
